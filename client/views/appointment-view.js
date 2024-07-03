596ac1982f882a40da46dd03e9e4fb497cf08542
pedrobemfica <pedrobemfica@gmail.com>
2024-07-03 14:52:01 +0000

    Test

--- client/views/appointment-view.js
+++ client/views/appointment-view.js
@@ -11,8 +11,8 @@ export class AppointmentView {
     initializeElements() {
 
         const inputService = document.getElementById('inputService');
-        const inputDate = document.getElementById('inputDate');
         const appointmentParamsForm = document.getElementById('appointmentParamsForm');
+        const appointmentNewButton = document.getElementById('appointmentNewButton');
 
         inputService.innerHTML = '';
         for (let service in SERVICES) {
@@ -22,14 +22,13 @@ export class AppointmentView {
             inputService.innerHTML += `<option ${seleted} value="${SERVICES[service].id}">${SERVICES[service].name}</option>`;
         }
 
-        const currentDate = new Date();
-        const today = currentDate.getFullYear() + '-' + currentDate.getMonth() + '-' + currentDate.getDay();
-        inputDate.setAttribute('value', today);
-        inputDate.value = today;
-
         appointmentParamsForm.addEventListener('submit', (event) => {
             event.preventDefault();
             this.listAvailability(inputDate.value.substring(0, 4), inputDate.value.substring(5, 7), inputDate.value.substring(8, 10), inputService.value);
+        })
+
+        appointmentNewButton.addEventListener('submit', (event) => {
+            event.preventDefault();
         })
     }
 
@@ -44,30 +43,33 @@ export class AppointmentView {
         }
     }
 
-    updateList(user_id) {
-        const appointmentList = document.getElementById('appointmentList');
-    }
-
     listAvailability(date_year, date_month, date_day, service) {
         const appointmentNewForm = document.getElementById('appointmentNewForm');
+        const appointmentNewButton = document.getElementById('appointmentNewButton');
 
         let availabilityList = mockAvailability.filter(e => (e.year == date_year && e.month == date_month && e.day == date_day && e.service == service));
-
+        console.log('hi');
         appointmentNewForm.innerHTML = '';
         for (let availability in availabilityList) {
-                            appointmentNewForm.innerHTML += `<div class="form-check">
-                                        <input class="form-check-input" type="radio" name="inputTimeRadio">
-                                        <label class="form-check-label" for="inputTimeRadio">
-                                            ${availabilityList[availability].hour} : ${availabilityList[availability].minute}
-                                        </label>
-                                        </div>`
+            let timeString = ("0" + availabilityList[availability].hour).slice(-2) + ":" + ("0" + availabilityList[availability].minute).slice(-2);
+            appointmentNewForm.innerHTML += `<div class="form-check">
+                                                <input class="form-check-input" type="radio" name="inputTimeRadio" value="${timeString}">
+                                                <label class="form-check-label" for="inputTimeRadio">
+                                                    ${timeString}
+                                                </label>
+                                            </div>`
         }
+        
         if (availabilityList.length != 0) {
-            appointmentNewForm.innerHTML += `<button type="submit" class="btn btn-primary " id="appointmentNewButton">Agendar</button>`
-            const appointmentNewButton = document.getElementById('appointmentNewButton');
-            appointmentNewButton.addEventListener('submit', (event) => {
-                event.preventDefault();
-            })
+            console.log('show');
+            appointmentNewButton.classList.remove("hidden");
+        } else {
+            console.log('hide');
+            appointmentNewButton.classList.add("hidden");
         }
+    }
+
+    updateList(user_id) {
+        const appointmentList = document.getElementById('appointmentList');
     }
 }
