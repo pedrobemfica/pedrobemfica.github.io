010ff4a02b362420edf3b5a8d602a35c4eb88baa
pfbp <pedrobemfica@yahoo.com.br>
2024-08-23 17:16:29 -0300

    Route to azure

--- app/api/cart-routes.js
+++ app/api/cart-routes.js
@@ -1,59 +1,124 @@
+import { BACKEND } from "./routes.js"
+import { Cookies } from "../helpers/cookie-helper.js"
+
+const BASE_ROUTE = '/cart'
 export class ApiCart {
-    static async get(userId, jwt) {
-        const response = await fetch('backend/cart/get', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ userId, jwt })
-        })
-        if (response.ok) {
-            const data = await response.json()
-            let cart = data.cart
-            return cart
-        }
-        else
-            return false
-    }
+
+    static async listCart() {
+        const token = Cookies.getCookie('jwt')
+        try {
+            const response = await fetch(`${BACKEND}${BASE_ROUTE}/list`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+            })
     
-    static async add(product, userId, jwt) {
-        const response = await fetch('backend/cart/add', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ product, userId, jwt })
-        })
-        if (response.ok) {
             const data = await response.json()
-            let cart = data.cart
-            return cart
+    
+            if (!data)
+                return {result: false, message: 'Não foi possível conectar com o servidor'}
+            
+            if (response.ok) 
+                return {result: true, list: data.list}
+            else 
+                return {result: false, message: data.message}
+            
+        } catch {
+            return {result: false, message: 'Não foi possível conectar com o servidor'}
         }
-        else
-            return false
     }
 
-    static async remove(index, userId, jwt) {
-        const response = await fetch('backend/cart/remove', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ index, userId, jwt })
-        })
-        if (response.ok) {
+    static async addSingleToCart(singleItem) {
+        const token = Cookies.getCookie('jwt')
+        try {
+            const response = await fetch(`${BACKEND}${BASE_ROUTE}/add-single`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+                body: JSON.stringify({ singleItem })
+            })
+    
             const data = await response.json()
-            let cart = data.cart
-            return cart
+    
+            if (!data)
+                return {result: false, message: 'Não foi possível conectar com o servidor'}
+            
+            if (response.ok)
+                return {result: true, message: data.message}
+            else 
+                return {result: false, message: data.message}
+            
+        } catch {
+            return {result: false, message: 'Não foi possível conectar com o servidor'}
         }
-        else
-            return false
     }
 
-    static async clear(userId, jwt) {
-        const response = await fetch('backend/cart/clear', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ userId, jwt })
-        })
-        if (response.ok) 
-            return true
-        else
-            return false
+    static async addPackageToCart(packageItem) {
+        const token = Cookies.getCookie('jwt')
+        try {
+            const response = await fetch(`${BACKEND}${BASE_ROUTE}/add-package`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+                body: JSON.stringify({ packageItem })
+            })
+    
+            const data = await response.json()
+    
+            if (!data)
+                return {result: false, message: 'Não foi possível conectar com o servidor'}
+            
+            if (response.ok)
+                return {result: true, message: data.message}
+            else 
+                return {result: false, message: data.message}
+            
+        } catch {
+            return {result: false, message: 'Não foi possível conectar com o servidor'}
+        }
     }
 
+    static async deleteFromCart(itemIndex) {
+        const token = Cookies.getCookie('jwt')
+        try {
+            const response = await fetch(`${BACKEND}${BASE_ROUTE}/delete`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+                body: JSON.stringify({ itemIndex })
+            })
+    
+            const data = await response.json()
+    
+            if (!data)
+                return {result: false, message: 'Não foi possível conectar com o servidor'}
+            
+            if (response.ok)
+                return {result: true, message: data.message}
+            else 
+                return {result: false, message: data.message}
+            
+        } catch {
+            return {result: false, message: 'Não foi possível conectar com o servidor'}
+        }
+    }
+
+    static async clearCart() {
+        const token = Cookies.getCookie('jwt')
+        try {
+            const response = await fetch(`${BACKEND}${BASE_ROUTE}/clear`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+            })
+    
+            const data = await response.json()
+    
+            if (!data)
+                return {result: false, message: 'Não foi possível conectar com o servidor'}
+            
+            if (response.ok) 
+                return {result: true, list: data.list}
+            else 
+                return {result: false, message: data.message}
+            
+        } catch {
+            return {result: false, message: 'Não foi possível conectar com o servidor'}
+        }
+    }
 }
